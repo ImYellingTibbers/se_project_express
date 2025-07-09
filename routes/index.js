4c4cb1984f6b18b0203e50845a0b39bdfebd65bb
Jacob Pixton <jacobcpix@gmail.com>
2025-07-09 16:30:31 -0600

    bugfix 404 error on server

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,11 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const { login, createUser } = require("../controllers/users");
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
 const {
-  OK,
-  CREATED,
-  BAD_REQUEST,
-  UNAUTHORIZED,
-  FORBIDDEN,
-  NOT_FOUND,
+  NOT_FOUND
 } = require("../utils/errors");
-
-console.log("SERVER STATUS CODES AT RUNTIME:", {
-  OK,
-  CREATED,
-  BAD_REQUEST,
-  UNAUTHORIZED,
-  FORBIDDEN,
-  NOT_FOUND,
-});
 
 router.post("/signin", login);
 router.post("/signup", createUser);
